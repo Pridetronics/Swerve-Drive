48b2da16beba6de09aa70c91f115d909e1dc855e
Matthew Brown <matthew.brown@pridetronics.com>
2023-11-03 15:26:06 -0700

    Finished Swerve Drive Auto

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,11 +6,15 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.SwerveModuleClasses;
+import frc.robot.Constants.WheelConstants;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -37,6 +41,13 @@ public class SwerveSubsystem extends SubsystemBase {
   );
 
   private AHRS gyro = new AHRS(SPI.Port.kMXP);
+  private final SwerveDriveOdometry odometer = new SwerveDriveOdometry(WheelConstants.kDriveKinematics,
+  new Rotation2d(0), new SwerveModulePosition[] {
+    frontLeft.getSwervePosition(),
+    frontRight.getSwervePosition(),
+    backLeft.getSwervePosition(),
+    backRight.getSwervePosition()
+  });
 
   /** Creates a new SwerveSubsystem. */
   public SwerveSubsystem() {
@@ -60,9 +71,30 @@ public class SwerveSubsystem extends SubsystemBase {
     return Rotation2d.fromDegrees(getHeading());
   }
 
+  public Pose2d getPose() {
+    return odometer.getPoseMeters();
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    odometer.resetPosition(getRotation2d(), new SwerveModulePosition[] {
+      frontLeft.getSwervePosition(),
+      frontRight.getSwervePosition(),
+      backLeft.getSwervePosition(),
+      backRight.getSwervePosition()
+    }, pose);
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    odometer.update(getRotation2d(), new SwerveModulePosition[] {
+      frontLeft.getSwervePosition(),
+      frontRight.getSwervePosition(),
+      backLeft.getSwervePosition(),
+      backRight.getSwervePosition()
+    });
+    SmartDashboard.putString("Robot Location", getPose().getTranslation().toString());
     SmartDashboard.putNumber("Robot Heading", getHeading());
   }
 
