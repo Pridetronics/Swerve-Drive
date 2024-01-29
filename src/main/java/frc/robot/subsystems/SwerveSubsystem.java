51baa57e0ac788b397c9b13c169c370da3ef503e
Matthew-PT <122941444+Matthew-PT@users.noreply.github.com>
2024-01-28 19:32:37 -0700

    Comments in SwerveSubsystem

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -21,9 +21,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsystem extends SubsystemBase {
-
+  //gets the constants for each module
   private static final SwerveModuleClasses SwerveModeuleSettings = new SwerveModuleClasses();
-
+  //Creates an object for each module
   private final SwerveModule frontLeft = new SwerveModule(
     SwerveModeuleSettings.frontLeft
   );
@@ -40,7 +40,9 @@ public class SwerveSubsystem extends SubsystemBase {
     SwerveModeuleSettings.backRight
   );
 
+  //Gets the NavX gyro to tell what direction the robot is facing
   private AHRS gyro = new AHRS(SPI.Port.kMXP);
+  //Uses the positions of each module to perdict where the robot is on the field (Mainly for autonomous)
   private final SwerveDriveOdometry odometer = new SwerveDriveOdometry(WheelConstants.kDriveKinematics,
   new Rotation2d(0), new SwerveModulePosition[] {
     frontLeft.getSwervePosition(),
@@ -48,9 +50,11 @@ public class SwerveSubsystem extends SubsystemBase {
     backLeft.getSwervePosition(),
     backRight.getSwervePosition()
   });
-
+  
   /** Creates a new SwerveSubsystem. */
   public SwerveSubsystem() {
+    //Waits one second to let the gyro calibrate and then resets the forward direction
+    //Non-yielding code
     new Thread(() -> {
       try {
         Thread.sleep(1000);
@@ -59,22 +63,33 @@ public class SwerveSubsystem extends SubsystemBase {
     }).start();
   }
 
+  //Resets the forward direction to the current forward direction of the robot
   public void zeroHeading() {
     gyro.reset();
   }
 
+  //Sets the robot heading to be what it currently is on the field, useful if the robot is not aligned with the forward direction of the field at the start of the match
+  public void setHeading(double currentRobotHeadingDegrees) {
+    zeroHeading();
+    gyro.setAngleAdjustment(currentRobotHeadingDegrees);
+  }
+
+  //Returns the heading of the robot on the field in degrees
   public double getHeading() {
     return gyro.getAngle() % 360;
   }
 
+  //Gets the rotation of the robot for use by WPILIB systems
   public Rotation2d getRotation2d() {
     return Rotation2d.fromDegrees(getHeading());
   }
 
+  //Gets the position + rotation of the robot for use by WPILIB systems
   public Pose2d getPose() {
     return odometer.getPoseMeters();
   }
 
+  //Resets the odometry to the robots current position and orientation (does NOT reset the gyro, just the odometer)
   public void resetOdometry(Pose2d pose) {
     odometer.resetPosition(getRotation2d(), new SwerveModulePosition[] {
       frontLeft.getSwervePosition(),
@@ -88,16 +103,7 @@ public class SwerveSubsystem extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
 
-    // SmartDashboard.putNumber("Module [9] Absolute", frontLeft.getAbsoluteEncoderRad() *(180/Math.PI) );
-    // SmartDashboard.putNumber("Module [10] Absolute", frontRight.getAbsoluteEncoderRad() *(180/Math.PI) );
-    // SmartDashboard.putNumber("Module [11] Absolute", backRight.getAbsoluteEncoderRad() *(180/Math.PI) );
-    // SmartDashboard.putNumber("Module [12] Absolute", backLeft.getAbsoluteEncoderRad() *(180/Math.PI) );
-
-    // SmartDashboard.putNumber("Module [9] Relative", frontLeft.getTurningPosition() *(180/Math.PI) );
-    // SmartDashboard.putNumber("Module [10] Relative", frontRight.getTurningPosition() *(180/Math.PI) );
-    // SmartDashboard.putNumber("Module [11] Relative", backRight.getTurningPosition() *(180/Math.PI) );
-    // SmartDashboard.putNumber("Module [12] Relative", backLeft.getTurningPosition() *(180/Math.PI) );
-
+    //Tells the odometer the current position to compare with the last position and thus update its predicted position
     odometer.update(getRotation2d(), new SwerveModulePosition[] {
       frontLeft.getSwervePosition(),
       frontRight.getSwervePosition(),
@@ -108,6 +114,7 @@ public class SwerveSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Robot Heading", getHeading());
   }
 
+  //Stops all the modules
   public void stopModules() {
     frontLeft.stop();
     frontRight.stop();
@@ -115,6 +122,7 @@ public class SwerveSubsystem extends SubsystemBase {
     backRight.stop();
   }
 
+  //Sets all the module's velocities and directions with an array of states
   public void setModuleStates(SwerveModuleState[] desiredStates) {
     SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
     frontLeft.setDesiredState(desiredStates[0]);
